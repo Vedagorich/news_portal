e15b709ce08c60c91235256c25b1bfde81bdd2f5
Vedagorich <yas.taras@mail.ru>
2022-11-14 17:06:31 +0300

    D6 сборка

---
+++ test.py
@@ -0,0 +1,7 @@
+# команды для быстрого набора
+
+#  python manage.py runserver     - запуск сайта
+#  python manage.py startapp simpleapp  - добавление нового приложения simpleapp
+#  django-admin startproject project  - создать новый проект project
+#  python3 manage.py makemigrations    - создать миграции
+#  python3 manage.py migrate - отправить миграцию в БД
